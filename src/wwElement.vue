9bee16c89e8eea019930d30ffa642ba67d29485c
CorentinBallaz <corentin.ballaz@steady-sun.com>
2022-02-24 17:30:35 +0100

    add some view components

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,96 +1,143 @@
 <template>
-    <div>
-        <!-- <highcharts :constructorType="'stockChart'" class="hc" :options="chartOptions" ref="chart"></highcharts> -->
-        <highcharts
-            class="stock-chart"
-            :constructor-type="'stockChart'"
-            :options="chartOptions"
-            :updateArgs="[true, false]"
-            ref="highcharts"
-        ></highcharts>
-    </div>
+  <div>
+    <highcharts
+      class="stock-chart"
+      :constructor-type="'stockChart'"
+      :options="chartOptions"
+      :updateArgs="[true, false]"
+      ref="highcharts"
+    ></highcharts>
+  </div>
 </template>
 
 <script>
-import { Chart } from 'highcharts-vue';
-import Highcharts from 'highcharts';
-import stockInit from 'highcharts/modules/stock';
+import { Chart } from "highcharts-vue";
+import Highcharts from "highcharts";
+import highchartsMore from "highcharts/highcharts-more";
+import stockInit from "highcharts/modules/stock";
+import exportingInit from "highcharts/modules/exporting";
+import HC_exportData from "highcharts/modules/export-data";
 
 stockInit(Highcharts);
+highchartsMore(Highcharts);
+exportingInit(Highcharts);
+HC_exportData(Highcharts);
 
 export default {
-    components: { highcharts: Chart },
-    props: { content: { type: Object, required: true } },
-    data() {
-        return {
-            updateArgs: [true, true, true],
-        };
-    },
-    computed: {
-        chartOptions() {
-            return {
-                chart: {
-                    spacingTop: 20,
-                    spacingBottom: 20,
-                    spacingLeft: 20,
-                    spacingRight: 20,
-                },
-                title: {
-                    text: this.content.title,
-                },
-                subtitle: {
-                    text : this.content.subtitle,
-                },
-                rangeSelector: {
-                    buttons: [
-                        {
-                            count: 12,
-                            type: 'hour',
-                            text: '12h',
-                        },
-                        {
-                            count: 1,
-                            type: 'day',
-                            text: '1d',
-                        },
-                        {
-                            count: 3,
-                            type: 'day',
-                            text: '3d',
-                        },
-                        {
-                            count: 5,
-                            type: 'day',
-                            text: '5d',
-                        },
-                        {
-                            type: 'all',
-                            text: 'All',
-                        },
-                    ],
-                },
-                plotOptions: {
-                    series: {
-                        showInNavigator: true,
-                    },
-                },
-                yAxis: {
-                    title: {
-                        text: this.content.yAxisTitle,
-                    },
-                    min: this.content.yAxisMin,
-                    opposite: false,
-                },
-                series: [
-                    {
-                        name: this.content.seriesName,
-                        data: this.content.data,
-                        color: this.content.seriesColor,
-                        lineWidth: this.content.lineWidth,
-                    },
-                ],
-            };
+  components: { highcharts: Chart },
+  props: { content: { type: Object, required: true } },
+  data() {
+    return {
+      updateArgs: [true, true, true],
+    };
+  },
+  computed: {
+    chartOptions() {
+      return {
+        chart: {
+          spacingTop: 20,
+          spacingBottom: 20,
+          spacingLeft: 20,
+          spacingRight: 20,
         },
+        title: {
+          text: this.content.title,
+        },
+        subtitle: {
+          text: this.content.subtitle,
+        },
+        rangeSelector: {
+          buttonTheme: {
+            // styles for the buttons
+            fill: "none",
+            stroke: "none",
+            "stroke-width": 0,
+            r: 8,
+            style: {
+              color: "#039",
+            },
+            states: {
+              hover: {},
+              select: {
+                fill: "#039",
+                style: {
+                  color: "white",
+                },
+              },
+              // disabled: { ... }
+            },
+          },
+          labelStyle: {
+            color: "silver",
+            fontWeight: "bold",
+          },
+          buttons: [
+            {
+              count: 12,
+              type: "hour",
+              text: "12h",
+            },
+            {
+              count: 1,
+              type: "day",
+              text: "1d",
+            },
+            {
+              count: 3,
+              type: "day",
+              text: "3d",
+            },
+            {
+              count: 5,
+              type: "day",
+              text: "5d",
+            },
+            {
+              type: "all",
+              text: "All",
+            },
+          ],
+        },
+        plotOptions: {
+          series: {
+            showInNavigator: true,
+          },
+        },
+        yAxis: {
+          title: {
+            text: this.content.yAxisTitle,
+          },
+          min: this.content.yAxisMin,
+          opposite: false,
+        },
+        series: this.content.series,
+        exporting: {
+          buttons: {
+            contextButton: {
+              menuItems: [
+                "viewFullscreen",
+                "printChart",
+                "separator",
+                "downloadPNG",
+                "downloadJPEG",
+                "downloadPDF",
+                "downloadSVG",
+                "separator",
+                "downloadCSV",
+                "downloadXLS",
+              ],
+            },
+          },
+        },
+      };
     },
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.stock-chart {
+  width: 100%;
+  height: 100%;
+}
+</style>
